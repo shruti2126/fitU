825d5df05c2317c2769c37ce51a0a12d2e35ff61
Shruti Sharma <shrutisharma@loan-rds-a387-x.lan>
2021-10-26 15:28:58 -0500

    login, sign up and signout functionality working

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,54 @@
+
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import {getAuth} from 'firebase/auth'
+import { NavigationRouteContext, useNavigation } from '@react-navigation/core'
 
-const HomeScreen = () => {
+type homeScreenProps = {
+    navigation: any;
+  };
+
+const HomeScreen: React.FC<homeScreenProps> = ({navigation}) => {
+    navigation = useNavigation()
+    const auth = getAuth()
+    const signOut = () => {
+        auth.signOut()
+        .then(() => {
+            navigation.replace("Login")
+        })
+    }
     return (
-        <View >
-           <Text>HOME</Text>
+        <View style={styles.container}>
+          <Text>Email: {auth.currentUser?.email}</Text>
+          <TouchableOpacity
+            onPress={signOut}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Sign out</Text>
+          </TouchableOpacity>
         </View>
-    )
-}
-
-export default HomeScreen
-
-const styles = StyleSheet.create({})
+      )
+    }
+    
+    export default HomeScreen
+    
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+       button: {
+        backgroundColor: '#0782F9',
+        width: '60%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 40,
+      },
+      buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
+      },
+    })
