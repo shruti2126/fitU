5549735c6f7a1414d8b631d3ee12caa7dcff80f5
Akshay Bodla <akshaybodla21@gmail.com>
2021-10-28 13:02:27 -0500

    flat list for rendering goals, maybe show only two items?

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -1,18 +1,59 @@
 import React from 'react'
-import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native'
+import { View, StyleSheet, Text, TouchableOpacity, Image, FlatList } from 'react-native'
 
 type homeScreenProps = {
     card_title?: string;
 };
 
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
+
+  const Item = ({ title }) => (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  );
+
 const ProfileCard: React.FC<homeScreenProps> = ({ 
     card_title,
 }) => {
+    const renderItem = ({ item }) => (
+        <Item title={item.title} />
+    );
+
     return (
         <View style={styles.container}>
             <Text style={styles.text_title}>Today's Progress</Text>
             <Text style={styles.text_body}>Steps Walked: </Text>
             <Text style={styles.text_body}>Hours Slept Last night: </Text>
+
+            <Text style={[
+                styles.text_title,
+                {
+                    marginTop: 5
+                }
+            ]}>
+                Daily Goals
+            </Text>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            />
+
+            <Text style={styles.text_body}>See more </Text>
         </View>
     )
 }
